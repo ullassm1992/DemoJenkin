a4f488001bf6d78418dd8eac31791cabb6458ea5
ullas <ullas@ullas-PC>
2020-10-19 20:38:23 +0530

    First build

---
+++ DemoJenkin/src/test/java/com/Jenkin/DemoTest/LoginTest.java
@@ -0,0 +1,13 @@
+package com.Jenkin.DemoTest;
+
+import org.testng.annotations.Test;
+
+public class LoginTest 
+{
+  @Test
+  public void logintest() 
+  {
+	  System.out.println("UN & PW validated : loggedin successfully!!!!");
+	  
+  }
+}
